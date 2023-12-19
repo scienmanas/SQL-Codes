53e7edc743414aebfd17833ae1c6f434af6a7632
Manas Poddar <iamscientistmanas@gmail.com>
2023-12-19 23:08:43 +0530

    updates notes

---
+++ SQL-Codes.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE temp1;
+
+CREATE DATABASE temp2;
+
+CREATE DATABASE IF NOT EXISTS college;
+
+DROP DATABASE temp1;
+DROP DATABASE temp2;
+
+USE college;
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+)
+
+INSERT INTO student Values(1, "Manas", 20);
+INSERT INTO student Values(2, "Mayank", 15);
+
+SELECT * FROM student;
+
+-- SHOW DATABASES;
+SHOW TABLES;
+
+DROP DATABASE IF EXISTS company;
